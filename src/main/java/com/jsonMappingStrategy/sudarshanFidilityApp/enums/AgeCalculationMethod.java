94093905a4fe1635e13a43228fd72e12623f7dc2
Bibash7 <biswash166@gmail.com>
2024-02-03 21:49:40 +0545

    ading matchers

--- src/main/java/com/jsonMappingStrategy/sudarshanFidilityApp/enums/AgeCalculationMethod.java
+++ src/main/java/com/jsonMappingStrategy/sudarshanFidilityApp/enums/AgeCalculationMethod.java
@@ -1,15 +1,15 @@
 package com.jsonMappingStrategy.sudarshanFidilityApp.enums;
 
 public enum AgeCalculationMethod {
-    ACM("AGE_COMPLETED_MONTHS"),
-    ANMD("AGE_NEAREST_MONTH_INCLUDE_DAYS"),
-    AA("AGE_ATTAINED"),
-    ACY("AGE_CALENDAR_YEAR"),
-    AFOMF("AGE_FIRST_OF_MONTH"),
-    AEOM("AGE_END_OF_MONTH"),
-    ALB("AGE_LAST_BIRTHDAY"),
-    ANM("AGE_NEAREST_MONTH"),
-    ANB("AGE_NEAREST_YEAR");
+    AGE_COMPLETED_MONTHS("ACM"),
+    AGE_NEAREST_MONTH_INCLUDE_DAYS("ANMD"),
+    AGE_ATTAINED("AA"),
+    AGE_CALENDAR_YEAR("ACY"),
+    AGE_FIRST_OF_MONTH("AFOMF"),
+    AGE_END_OF_MONTH("AEOM"),
+    AGE_LAST_BIRTHDAY("ALB"),
+    AGE_NEAREST_MONTH("ANM"),
+    AGE_NEAREST_YEAR("ANB");
 
     private final String code;
 
